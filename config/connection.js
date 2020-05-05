d741483372a653a897052526efbcb097a4a335b7
kcenters <kristincenters@gmail.com>
2020-05-05 17:57:06 -0500

    changed config var

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,13 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PW,
-    port: 3306,
-    database: "burgers_db"
-});
+var connection = mysql.createConnection(
+    {
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PW,
+        port: 3306,
+        database: process.env.DB_NAME
+    });
 
 connection.connect(function (err) {
     if (err) {
